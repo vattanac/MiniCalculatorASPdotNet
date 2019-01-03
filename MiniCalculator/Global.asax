e7009b4f3c42c7c732633e645bc9f2b719052569
vattanac <vattanacsim99@gmail.com>
2019-01-04 06:41:03 +0700

    This is a new commit for what I originally planned to be amended

---
+++ MiniCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniCalculator.MvcApplication" Language="C#" %>
---
+++ MiniCalculator/Models/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniCalculator.Models
+{
+    public class Calculator
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double result { get; set; }
+    }
+}
